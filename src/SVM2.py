cd57cebe77ea94f6883b29887e62bf1cd64414cc
mazzma12 <matthieu.mazzolini@gmail.com>
2017-03-08 20:12:42 +0100

    OVO is working according to a real decoding from Schapire

--- src/SVM2.py
+++ src/SVM2.py
@@ -15,11 +15,29 @@ solvers.options['show_progress'] = False # Verbose quite
 from itertools import product
 
 
+# Scientific libraries
+import numpy as np
+import pandas as pd
+from numpy import linalg
+from numpy import random
+import scipy
+from scipy.spatial.distance import pdist, cdist, squareform
+from scipy.stats import mode
+
+# cvxopt QP solver
+import cvxopt
+from cvxopt import solvers, matrix
+solvers.options['show_progress'] = False # Verbose quite
+
+from itertools import product
+
+
 class SVM2:
     kernels_ = ['linear', 'rbf', 'Cauchy', 'poly', 'TStudent', 'cosine', 'GHI']
     losses_ = ['hinge', 'squared_hinge']
     
-    def __init__(self, C=1, kernel='rbf', gamma=0.1, mode='OVA', loss='squared_hinge', c=0, degree=2):
+    def __init__(self, C=1, kernel='poly', gamma=0.1, mode='OVO', loss='squared_hinge', c=0.159, degree=4, 
+                 decoding='loss-based'):
         self.C = C
         self.kernel = kernel # kernel_function 'rbf', 'linear'
         self.gamma = gamma # Kernel coefficient gamma for 'rbf'
@@ -28,6 +46,7 @@ class SVM2:
         self.c = c # Intercept of the polynomial kernel
         self.degree = degree # Degree of the polynomial kernel
         self.alphas_ = [] # coefficients of the estimators
+        self.decoding_ = decoding # Way of decoding the coding matrix 
 
     def fit(self, X, y, ):
         
@@ -120,15 +139,15 @@ class SVM2:
         
         elif self.mode == 'OVO':
             
-            # calling OVO_estimators_masks() to built masks_estimators
-            # Used in masks_estimators*K_test later
-            # rmk : OVO_estimators_masks() is always the same, but cheap to compute
-            self.masks_estimators_ = OVO_estimators_masks()            
+            # calling coding_matrix() to built a matrix that would select the correct configuration for OVO
+            # rmk : coding_matrix() is always the same, but cheap to compute
+            self.coding_matrix_ = coding_matrix()            
             
             estimators_preds = [] # list of length n_estimators which contains the predictions
             # n_estimators = 45 in OVO : n_classes_(n_classes-1)/2
             # each estimators  is of size n_test
             
+            #-------CHOOSING THE BEST PREDICTION FOR EACH CLASS------
             for idx, mask in enumerate(self.masks_samples_):
                 alpha = self.alphas_[idx]
                 # omega = alpha*self.y_copies_[idx] not useful
@@ -138,24 +157,23 @@ class SVM2:
             estimators_preds = np.sign(np.array(estimators_preds))
             # estimators_preds is now an array of size n_estimators*n_test
             
-            class_preds = np.dot(self.masks_estimators_, estimators_preds)
-            y_pred = class_preds.argmax(0)
             
-            """# Choosing which class is predicted using the mask_matrix built with OVO_idx_class()
+            #------DECODING PART-----------
             classes_preds = [] # list of length n_classes_, contain the predictions for each class
-            for mask in self.masks_estimators_:
+            for mask in self.coding_matrix_:
                 class_pred = estimators_preds.copy()
-                class_pred = class_pred*mask.reshape(-1,1)
-                class_pred[class_pred < 0] = 0
-                class_pred = class_pred.sum(0)
+                
+                if self.decoding_ == 'Hamming':
+                    class_pred = (1 - np.sign(class_pred*mask.reshape(-1,1))).sum(0)
+                    
+                elif self.decoding_ == 'loss-based':
+                    class_pred = ((np.maximum(1 - class_pred*mask.reshape(-1,1), 0))**2).sum(0)
+                    
                 classes_preds.append(class_pred)
             
             classes_preds = np.array(classes_preds)
-            
-            # Argmax give the index of the row with the highest score
-            # rows are ordered so the index corresponds to the class
-            y_pred = classes_preds.argmax(0)"""
-            
+            self.classes_preds = classes_preds
+            y_pred = classes_preds.argmin(0) # Not argmax, because it's loss
             return y_pred
 
     def fit_kernel(self):
@@ -163,6 +181,7 @@ class SVM2:
         X = self.X_train_
         
         if self.kernel == 'rbf':
+            print('rbf choosen')
             pairwise_dists = squareform(pdist(X, 'euclidean'))
             K = scipy.exp(-self.gamma*pairwise_dists ** 2)
         
@@ -199,7 +218,7 @@ class SVM2:
             # Also known as the tangent hyperbolic
             #There are two adjustable parameters in the sigmoid kernel
             #the slope alpha and the intercept constant c. 
-            #A common value for alpha is 1/N, where N is the data dimension.
+            #A common value for gamma is 1/N, where N is the data dimension.
             pairwise_dists = squareform(pdist(X, 'cosine'))
             K = scipy.tanh(self.gamma*np.dot(X, X.transpose()) + self.c)
         elif self.kernel == 'min':
@@ -208,6 +227,12 @@ class SVM2:
         elif self.kernel == 'GHI':
             # Error QP : Rank(A) < p or Rank([P; A; G]) < n
             K = squareform(pdist(X, lambda u, v : np.sum(scipy.minimum(np.abs(u)** self.gamma, np.abs(v)** self.gamma))))
+        
+        elif isinstance(self.kernel, list):
+            pairwise_dists = squareform(pdist(X, 'euclidean'))
+            K = 0.1*scipy.exp(-self.gamma*pairwise_dists ** 2)
+            K += 0.9*(self.c + np.dot(X, X.transpose())) ** self.degree
+                
         
         else:         
             raise Exception('the kernel must either be rbf or linear')
@@ -259,7 +284,11 @@ class SVM2:
         elif self.kernel == 'GHI':
             K = cdist(X_test, self.X_train_, 
                       lambda u, v : np.sum(scipy.minimum(np.abs(u)** self.gamma, np.abs(v)** self.gamma)))
-                          
+        elif isinstance(self.kernel, list):
+            pairwise_dists = cdist(X_test, self.X_train_)
+            K = 0.1*scipy.exp(-self.gamma*pairwise_dists ** 2)
+            K += 0.9*(self.c + np.dot(X_test, self.X_train_.transpose())) ** self.degree
+
         else:
             raise Exception('the kernel must either be rbf or linear')
         
@@ -282,7 +311,7 @@ class SVM2:
             
             diag_y = np.diag(y)
             self.p_ = (-y)
-            self.Q_ = self.C*n*np.eye(n) # Quadratic matrix
+            self.Q_ = self.C*n*np.eye(n) # Quadratic matrix, need + K, added in fit()
             self.G_ = -diag_y # Constraint matrix of size(2*n, n)
             self.h_ = np.zeros(n)
             
@@ -295,9 +324,9 @@ class SVM2:
         # Describe the train set
         print(" The Data contain ", self.n_classes_, " classes, and the repartion is :  ", self.repartition_)
 
-def OVO_estimators_masks():
-    # DIFFICULT
-    # Build a matrix with 1 and 0 for the OVO prediction
+def coding_matrix():
+    """Computation of the coding matrix for the OVO as referred to in Schapire e al. paper
+    http://www.jmlr.org/papers/volume1/allwein00a/allwein00a.pdf"""
     masks = []
     for n in range(10):    
         mask = []
@@ -315,11 +344,12 @@ def OVO_estimators_masks():
     masks = np.array(masks)
     return masks
 
-def tune_parameters(X, y, param_grid, n_train, X_test = None, verbose = True):
+def tune_parameters(X, y, X_val, y_val, param_grid, n_train, X_test = None, verbose = True):
     """X : array which would be split in train and val set according to n_train
     n_train : number of train samples. Integer or percentage
     param_grid : dict containing list of parameters to be tested
     IMPORTANT : param_grid values have to be a list. ex : not 'hinge' but ['hinge']
+    IMPORTANT 2 : We should pass X_val as an argument, because an image x is differentiate 4 times. Therefore if only 2 of them are in the train set, the 2 others can be in the validation one, and hence the score increase 
     """
     
     n_total = X.shape[0]
@@ -329,8 +359,8 @@ def tune_parameters(X, y, param_grid, n_train, X_test = None, verbose = True):
     
     # Storing results
     scores = {}
-    preds = []
-    preds_test = []
+    preds = {}
+    preds_test = {}
     estimators = {}
     param_grid = [param_grid]
     for param in param_grid:
@@ -343,27 +373,27 @@ def tune_parameters(X, y, param_grid, n_train, X_test = None, verbose = True):
             svm = SVM2(**params)
             
             # Checking if n_train is percentage or integer
-            if n_train < 1:
+            if n_train <= 1:
                 idx_train = random.choice(np.arange(n_total), int(n_train*n_total), replace=False)
             else :
                 idx_train = random.choice(np.arange(n_total), n_train, replace=False)
                
-            idx_val = list(set(np.arange(n_total)) - set(idx_train))
+            #idx_val = list(set(np.arange(n_total)) - set(idx_train))
             # n_val max is set to 2000
-            if len(idx_val) > 2000:
+            """if len(idx_val) > 2000:
                 idx_val = idx_val[:2000]
-            
+            """
             # Fitting and storing results
             svm.fit(X[idx_train], y[idx_train])
-            pred = svm.predict(X[idx_val])
+            pred = svm.predict(X_val)
             estimators[str(params)] = svm.alphas_
-            preds.append(pred)
-            score = np.mean(pred == y[idx_val])                
+            preds[str(params)] = pred
+            score = np.mean(pred == y_val)                
             scores[str(params)]= score
             
             if X_test is not None:
                     pred_test = svm.predict(X_test)
-                    preds_test.append(pred_test)
+                    preds_test[str(params)] = pred_test
             
             if verbose is True:
                 print(params)
